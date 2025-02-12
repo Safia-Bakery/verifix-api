57c504bd258a8cfb0af409c48ce53f7f4410238e
Gayratbek <gayratbekakhmedov@gmail.com>
2025-02-12 11:48:51 +0500

    edit working tim

--- verifix/routes/mindbox.py
+++ verifix/routes/mindbox.py
@@ -144,7 +144,7 @@ def prepareReport():
             except Exception as e:
                 print(f"Error deleting excell  {e}")
             try:
-                os.remove(f"{file_return['gz_file']}")
+                os.remove(file_return['gz_file'])
             except Exception as e:
                 print(f"Error deleting gz file {e}")
             updateIsSended(db=db,export_id=file_id)
@@ -154,7 +154,7 @@ def prepareReport():
 @mindbox_router.on_event("startup")
 def startup_event():
     scheduler = BackgroundScheduler()
-    trigger = CronTrigger(hour=10, minute=55, second=00,
+    trigger = CronTrigger(hour=9, minute=00, second=00,
                           timezone=timezone_tash)  # Set the desired time for the function to run (here, 12:00 PM)
     scheduler.add_job(prepareReport, trigger=trigger)
     scheduler.start()
@@ -165,7 +165,7 @@ def startup_event():
 @mindbox_router.on_event("startup")
 def startup_event_request_report():
     scheduler = BackgroundScheduler()
-    trigger = CronTrigger(hour=10, minute=51, second=00,
+    trigger = CronTrigger(hour=3, minute=00, second=00,
                           timezone=timezone_tash)  # Set the desired time for the function to run (here, 12:00 PM)
     scheduler.add_job(requestFileReportMindbox, trigger=trigger)
     scheduler.start()
